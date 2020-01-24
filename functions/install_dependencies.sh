2279e207bf79575135f854095ac1d42062413081
ingdevs <54769070+ingdevs@users.noreply.github.com>
2020-01-24 20:16:39 +0100

    Added curl

--- functions/install_dependencies.sh
+++ functions/install_dependencies.sh
@@ -28,7 +28,7 @@ printf -- "\e[0m\e[37m-> installing dependencies\e[0m";
 printf -- "\n";
 install_dependencies () {
 	sudo apt-get -y install git nano ntp ntpdate nginx php7.2-fpm php7.2-common php7.2-mbstring php7.2-xmlrpc php7.2-gd php7.2-xml php7.2-mysql php7.2-cli php7.2-zip php7.2-curl php7.2-soap php7.2-intl php7.2-ldap mariadb-server mariadb-client certbot python-certbot-nginx curl 1>>$logfile 2>>$errlog
-	printf -- "\e[90mfollowing dependencies have been isntalled: git nano ntp ntpdate nginx php7.2-fpm php7.2-common php7.2-mbstring php7.2-xmlrpc php7.2-gd php7.2-xml php7.2-mysql php7.2-cli php7.2-zip php7.2-curl php7.2-soap php7.2-intl php7.2-ldap mariadb-server mariadb-client certbot python-certbot-nginx\e[22m"
+	printf -- "\e[90mfollowing dependencies have been isntalled: git nano ntp ntpdate nginx php7.2-fpm php7.2-common php7.2-mbstring php7.2-xmlrpc php7.2-gd php7.2-xml php7.2-mysql php7.2-cli php7.2-zip php7.2-curl php7.2-soap php7.2-intl php7.2-ldap mariadb-server mariadb-client certbot python-certbot-nginx curl\e[22m"
 }
 install_dependencies
 echo ""
