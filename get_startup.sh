0db1a04e77bc0348067ee2e5490173a61571635b
fountainpositive <54769070+fountainpositive@users.noreply.github.com>
2020-03-05 22:17:48 +0100

    Fixing repo names

--- get_startup.sh
+++ get_startup.sh
@@ -3,6 +3,6 @@
 #fetch and run the startup script
 sudo apt install git -y
 sudo mkdir worplet && cd worplet
-sudo wget https://raw.githubusercontent.com/ingdevelopers/wordpress-droplet/master/startup.sh
+sudo wget https://raw.githubusercontent.com/fountainpositive/worplet/master/startup.sh
 sudo chmod +x startup.sh
 sudo time ./startup.sh
